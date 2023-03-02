e50022f123895352c40888d7ad558b844ee82f2f
Sergey <szaprudnov@hotmail.com>
2023-03-02 08:40:36 +0300

    add foo test list

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,8 @@ def test_json_load():
     assert type(utils.json_load()) == list
     assert len((utils.json_load())) > 0
 
+def test_st_executed():
+    """тестирование списка операций"""
+    assert isinstance(utils.st_executed(), list)
+    assert utils.st_executed() is not None
+
