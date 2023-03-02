0aa4e62878a6c2c8cddc374fb4db3dacd0072a39
Sergey <szaprudnov@hotmail.com>
2023-03-02 08:37:09 +0300

    add foo test json

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
 from foo import utils
-
-
 import requests
 import pytest
 
 
 def test_json_load():
+    """тестирование получения данных"""
     assert type(utils.json_load()) == list
     assert len((utils.json_load())) > 0
+
